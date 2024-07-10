27a84d238d08a48911274b83b7ff6669f3f948cd
Vedant-pilaniwala <babitabydesign@gmail.com>
2024-07-10 14:28:22 +0530

    New feature

--- To-do-app/src/App.jsx
+++ To-do-app/src/App.jsx
@@ -1,25 +1,26 @@
 import Header from "./Components/Header";
 import AddToDo from "./Components/Add-To-do";
 import ToDoLIst from "./Components/To-do-list";
+import NoTaskMsg from "./Components/No-task-msg";
+import { useState } from "react";
 
 function App() {
 
-  const toDoList = [
-    {
-      task: 'Buy Milk',
-      date: '08/07/24'
-    },
-    {
-      task: 'Complete the projects',
-      date: '08/07/24'
-    }
-  ]
+  const [toDoList, setToDoList] = useState([])
+
+  const handleOnNewItem = (newTask, newDueDate) => {
+    const newItemList = [...toDoList,
+    { task: newTask, date: newDueDate }
+    ];
+    setToDoList(newItemList);
+  }
 
   return (
     <div className="ml-auto mr-auto mt-5 border-2 border-solid rounded-3xl w-6/12 h-auto p-3 bg-gradient-to-r from-green-400 to-blue-500">
       <Header />
-      <AddToDo />
-      <ToDoLIst TodoItems={toDoList}/>
+      <AddToDo onNewItem={handleOnNewItem} />
+      {toDoList.length === 0 && <NoTaskMsg></NoTaskMsg> }
+      <ToDoLIst TodoItems={toDoList} />
     </div>
   );
 }
