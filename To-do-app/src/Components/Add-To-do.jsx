d7f5e527d67a3bb5d5e132c9df5403d3b426b5cd
Vedant-pilaniwala <babitabydesign@gmail.com>
2024-07-10 14:27:39 +0530

    New feature

--- To-do-app/src/Components/Add-To-do.jsx
+++ To-do-app/src/Components/Add-To-do.jsx
@@ -1,18 +1,51 @@
+import { useState } from "react";
 
+function AddToDo({ onNewItem }) {
 
-function AddToDo() {
+    const [newTask, setNewTask] = useState("");
+    const [newDueDate, setNewDueDate] = useState("");
 
+    const handleNewTask = (evt) => {
+        setNewTask(evt.target.value);
+    }
+
+    const handleNewDate = (evt) => {
+        setNewDueDate(evt.target.value);
+    }
+    
+    const handleOnAdd = () => {
+        onNewItem(newTask, newDueDate);
+        setNewDueDate("");
+        setNewTask("");
+    }
 
     return (
         <center>
             <input
-                className="border-2 border-gray-200 rounded-md m-4 p-2 w-5/12 bg-transparent font-semibold placeholder:text-sky-950" type="text" placeholder="Enter your task"
+                className="
+                border-2 border-gray-200 rounded-md m-4 p-2 w-5/12 bg-transparent font-semibold placeholder:text-sky-950
+                "
+                type="text"
+                placeholder="Enter your task"
+                value={newTask}
+                onChange={handleNewTask}
             />
             <input
-                className="border-2 border-gray-200 rounded-md m-4 p-2 w-3/12 bg-transparent font-semibold text-sky-950" type="date"
+                className="
+                border-2 border-gray-200 rounded-md m-4 p-2 w-3/12 bg-transparent font-semibold
+                text-sky-950
+                "
+                type="date"
+                value={newDueDate}
+                onChange={handleNewDate}
             />
             <button
-                className="rounded-md m-4 p-2 w-2/12 bg-gradient-to-r from-green-500 to-green-600 hover:brightness-110 active:brightness-125 text-2xl text-white"
+                className="
+                rounded-md m-4 p-2 w-2/12 bg-gradient-to-r from-green-500 to-green-600 hover:brightness-110 active:brightness-125 text-2xl text-white
+                "
+                onClick={handleOnAdd}
+                id="addBtn"
+                disabled = {newTask === "" && newDueDate === "" || newDueDate === "" || newTask === "" ? true : false}
             >
                 Add
             </button>
---
+++ To-do-app/src/Components/No-task-msg.jsx
@@ -0,0 +1,7 @@
+
+
+function NoTaskMsg() {
+    <h1 >Enjoy your day</h1>
+}
+
+export default NoTaskMsg
