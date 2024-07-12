937cc6ae45e47a48860ba45db075f807af727c77
Vedant-pilaniwala <babitabydesign@gmail.com>
2024-07-12 14:46:52 +0530

    My calculator app

--- Calculator/src/Components/Display.jsx
+++ Calculator/src/Components/Display.jsx
@@ -5,7 +5,7 @@ function Display() {
     return (
         <input
             type="text"
-            className="m-0 w-full bg-gray-950 pt-24 pr-4 text-white min-h-20 text-7xl
+            className="m-0 w-full bg-gray-950 pt-24 pr-4 text-white min-h-16 text-7xl
              outline-none text-end"
             value="0"
             readOnly
